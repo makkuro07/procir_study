9cf330af331b8584f34799e7b33a58418e742ec9
makkuro07 <kurotomo110702@gmail.com>
2022-07-04 20:48:25 +0900

    commit1回目

---
+++ kdai20.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>git</title>
+</head>
+<body>
+gitの勉強
+</body>
+</html>
---
+++ study.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>GET受信</title>
+</head>
+<body>
+gitのcommit
+</body>
+</html>
